2edfbe27eff4e73deccf6c65fbb2bbd9d327a25f
Robert Sandler <robert.f.sandler@gmail.com>
2012-08-24 13:13:06 -0700

    This is mostly just a test.

---
+++ Godly/src/com/github/roburrito/godly/Godly.java
@@ -0,0 +1,11 @@
+package com.github.roburrito.godly;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Godly extends JavaPlugin {
+
+	private void registerEvents() {
+		PluginManager pluginmanager = getServer().getPluginManager();
+	}
+}
