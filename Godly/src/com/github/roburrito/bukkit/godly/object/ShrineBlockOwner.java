d64860ed04ad41fe691be9c6d845fb3ca84ff440
Robert Sandler <robert.f.sandler@gmail.com>
2012-08-27 00:57:34 -0700

    ShrineBlockOwner now matches TownBlockOwner, with all practical differences in place.

---
+++ Godly/src/com/github/roburrito/bukkit/godly/object/ShrineBlockOwner.java
@@ -0,0 +1,47 @@
+package com.github.roburrito.bukkit.godly.object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.palmergames.bukkit.towny.exceptions.AlreadyRegisteredException;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+
+public class ShrineBlockOwner {
+
+	protected List<ShrineBlock> shrineBlocks = new ArrayList<ShrineBlock>();
+	protected GodlyPermission permissions;
+	
+	public void setShrineBlocks(List<ShrineBlock> shrineBlocks) {
+		this.shrineBlocks = shrineBlocks;
+	}
+	
+	public List<ShrineBlock> getShrineBlocks() {
+		return shrineBlocks;
+	}
+	
+	public boolean hasShrineBlock(ShrineBlock shrineBlock) {
+		return shrineBlocks.contains(shrineBlock);
+	}
+	
+	public void addShrineBlock(ShrineBlock shrineBlock) throws AlreadyRegisteredException {
+		if(hasShrineBlock(shrineBlock))
+			throw new AlreadyRegisteredException();
+		else
+			shrineBlocks.add(shrineBlock);
+	}
+	
+	public void removeShrineBlock(ShrineBlock shrineBlock) throws NotRegisteredException {
+		if(!hasShrineBlock(shrineBlock))
+			throw new NotRegisteredException();
+		else
+			shrineBlocks.remove(shrineBlock);
+	}
+	
+	public void setPermissions(String line) {
+		permissions.load(line);
+	}
+	
+	public GodlyPermission getPermissions() {
+		return permissions;
+	}
+}
